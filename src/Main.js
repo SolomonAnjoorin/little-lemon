6f77b84cca46618478c419cf82eef28149bc0b37
SolomonAnjoorin <solomonanjoorin@gmail.com>
2023-08-27 15:21:47 +0100

    sunday fourth commit

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import restauranfood from './restauranfood.jpg'; 
 
 function Main() {
   return (
     <main>
       <section>
-        <h3>Main Section</h3>
+        <h4>Main Section</h4>
         <p>This is the main content of the website.</p>
       </section>
+      <div className="image-container">
+        <img src={restauranfood} alt="Restaurant Food" />
+        <div className="image-caption">Hover to Zoom</div>
+      </div>
     </main>
   );
 }
